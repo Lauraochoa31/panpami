004deb0a50689e0aa689a200864d716abee3d0f0
Lauraochoa31 <lauraochoa666@gmail.com>
2024-08-27 19:37:39 -0500

    feat(243): Create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header/Header';
+import { LoadingView } from './LoadingView/LoadingView';
 
 function App() {
   return (
     <div className="App">
       <h1>Pan pa' mi</h1>
       <p>Estamos horneando algo muy especial</p>
+      <LoadingView />
+      <Header />
     </div>
     
   );
