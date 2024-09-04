a7fb6b44b8143542a2702f09c196a1bc01ef4b67
Lauraochoa31 <lauraochoa666@gmail.com>
2024-09-04 14:54:00 -0500

    layout-creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import './App.css';
-import Header from './Header/Header';
-import { LoadingView } from './LoadingView/LoadingView';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import { LoadingView } from "./LoadingView/LoadingView";
+
+const products = [
+  {
+    name: "pan huevito",
+    price: "1000",
+    type: "dulce",
+  },
+  {
+    name: "croissant",
+    price: "2000",
+  },
+];
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 6000);
+
+  const productsSweet = products.filter(product => product.type === "dulce")
+
   return (
     <div className="App">
       <h1>Pan pa' mi</h1>
       <p>Estamos horneando algo muy especial</p>
-      <LoadingView />
+      {loading && <LoadingView />}
       <Header />
+      {productsSweet.map((product: {price:string, name: string}) => (
+        <ul>
+          <li>{product.name}</li>
+          <li>{product.price}</li>
+        </ul>
+      ))}
     </div>
-    
   );
 }
 
